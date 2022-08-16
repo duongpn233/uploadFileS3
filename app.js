1f76916cf228176ca0ee3c9b1f1abd6abf49a5d5
NgocDuong <phamduongx9@gmail.com>
2022-08-16 22:27:48 +0700

    update v1

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const cros = require('cors');
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-    accessKeyId: 'UHhVVP04jUnR9aQAXUfG',
-    secretAccessKey: 'YkPNBFfCKxsImjUj2bfphNtJ02k3HaUN1ujNf62Y'
+    endpoint: process.env.ENDPOINT,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+    s3ForcePathStyle: true
 });
 
 const app = express();
@@ -14,17 +16,67 @@ app.use(cros());
 
 app.get("/sign-s3", (req, res, next) => {
     const s3 = new AWS.S3();
+    s3.putBucketCors(
+        {
+            Bucket: process.env.BUCKET,
+            CORSConfiguration: {
+                CORSRules: [
+                    {
+                        AllowedHeaders: [
+                            "*"
+                        ],
+                        AllowedMethods: [
+                            "PUT",
+                            "POST",
+                            "DELETE"
+                        ],
+                        AllowedOrigins: [
+                            "*"
+                        ]
+                    }
+                ]
+            }
+        },
+        err => {
+            if (err) console.log(err, err.stack);
+            else console.log(`Edit Bucket CORS succeed!`);
+        }
+    );
+    let readOnlyAnonUserPolicy = {
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Effect: "Allow",
+                Principal: "*",
+                Action: "*",
+                Resource: [
+                    ""
+                ],
+                ExposeHeaders: [],
+            }
+        ]
+    };
+    var bucketResource = "arn:aws:s3:::" + process.env.BUCKET + "/*";
+    readOnlyAnonUserPolicy.Statement[0].Resource[0] = bucketResource;
+    let bucketPolicyParams = { Bucket: process.env.BUCKET, Policy: JSON.stringify(readOnlyAnonUserPolicy) };
+    s3.putBucketPolicy(bucketPolicyParams, function (err, data) {
+        if (err) {
+            // display error message
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
     const fileName = req.query["file-name"];
     const fileType = req.query["file-type"];
-    console.log(process.env.BUCKET)
     const params = {
-        Bucket: 'traffic',
+        Bucket: process.env.BUCKET,
         Key: fileName,
-        Expires: 180,
+        Expires: 300,
         ContentType: fileType,
         ACL: "public-read"
     };
-    s3.getSignedUrl("putObject", params, (err, data) => {
+    s3.getSignedUrl('putObject', params, (err, data) => {
         if (err) {
             console.log(err);
             return next(err);
@@ -32,8 +84,84 @@ app.get("/sign-s3", (req, res, next) => {
         else {
             return res.status(200).json(data);
         }
-    })
+    });
 });
+
+app.get("/down-sign-s3", (req, res, next) => {
+    const s3 = new AWS.S3();
+    s3.putBucketCors(
+        {
+            Bucket: process.env.BUCKET,
+            CORSConfiguration: {
+                CORSRules: [
+                    {
+                        AllowedHeaders: [
+                            "*"
+                        ],
+                        AllowedMethods: [
+                            "PUT",
+                            "POST",
+                            "DELETE",
+                            "GET"
+                        ],
+                        AllowedOrigins: [
+                            "*"
+                        ]
+                    }
+                ]
+            }
+        },
+        err => {
+            if (err) console.log(err, err.stack);
+            else console.log(`Edit Bucket CORS succeed!`);
+        }
+    );
+
+    let readOnlyAnonUserPolicy = {
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Effect: "Allow",
+                Principal: "*",
+                Action: "*",
+                Resource: [
+                    ""
+                ],
+                ExposeHeaders: [],
+            }
+        ]
+    };
+    var bucketResource = "arn:aws:s3:::" + process.env.BUCKET + "/*";
+    readOnlyAnonUserPolicy.Statement[0].Resource[0] = bucketResource;
+    let bucketPolicyParams = { Bucket: process.env.BUCKET, Policy: JSON.stringify(readOnlyAnonUserPolicy) };
+    s3.putBucketPolicy(bucketPolicyParams, function (err, data) {
+        if (err) {
+            // display error message
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
+
+    const fileName = req.query["file-name"];
+    const fileType = req.query["file-type"];
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: fileName,
+        Expires: 180,
+        // ContentType: fileType,
+        // ACL: "public-read"
+    };
+    s3.getSignedUrl('getObject', params, (err, data) => {
+        if (err) {
+            console.log(err);
+            return next(err);
+        }
+        else {
+            return res.status(200).json(data);
+        }
+    });
+})
 
 app.use((err, req, res) => {
     const status = err.status || 500;
