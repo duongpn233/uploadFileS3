a990d6b4683b01355dfe2e2d68f8dd7735bdcc41
NgocDuong <phamduongx9@gmail.com>
2022-08-22 16:33:23 +0700

    update v3

--- app.js
+++ app.js
@@ -5,8 +5,24 @@ const AWS = require("aws-sdk");
 const fs = require('fs');
 const path = require('path');
 const Memcached = require('memcached');
+const { Buffer } = require('buffer');
+const { createClient } = require('redis');
+const bodyParser = require('body-parser');
+const uploadFile = require('./middleware/upFiles');
+const { uploadPart, createMultipart, completeUpload, abortUpload } = require('./util/uploadMultipart');
+
 
 const memcached = new Memcached("localhost:11211");
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis Client Error', err));
+async function connect() {
+    await client.connect();
+    const value = await client.get('test');
+    console.log(value);
+};
+connect();
+
 
 AWS.config.update({
     endpoint: process.env.ENDPOINT,
@@ -17,6 +33,8 @@ AWS.config.update({
 
 const app = express();
 app.use(cros());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/get-bucket", (req, res, next) => {
     const s3 = new AWS.S3();
@@ -226,44 +244,61 @@ app.get("/down-sign-s3", (req, res, next) => {
     });
 });
 
-app.get("/save-file", (req, res, next) => {
-    const s3 = new AWS.S3();
-    const fileName = req.query["file-name"];
-    memcached.get(fileName, (err, data) => {
-        if (data) {
-            console.log("Cached");
-            res.json({
-                type: "cache",
-                data: data
-            });
-        }
-        else if (err) {
-            next(err);
-        }
-        else {
-            const params = {
-                Bucket: process.env.BUCKET,
-                Key: fileName,
-            };
-            s3.getObject(params, function (err, data) {
-                if (err) next(err);
-                else {
-                    //const pathFile = path.join(__dirname, `./file/${fileName}`);
-                    memcached.set(fileName, data.Body, 600, (err) => {
-                        if (err) {
-                            next(err);
+app.get("/save-file", async (req, res, next) => {
+    try {
+        const s3 = new AWS.S3();
+        const fileName = req.query["file-name"];
+        let metadata = await client.hGetAll(fileName);
+        memcached.get(fileName, (err, data) => {
+            if (data) {
+                const dataRes = Buffer.from(data, 'base64');
+                res.json({
+                    type: "cache",
+                    data: dataRes,
+                    metadata: metadata
+                });
+            }
+            else if (err) {
+                next(err);
+            }
+            else {
+                const params = {
+                    Bucket: process.env.BUCKET,
+                    Key: fileName,
+                };
+                s3.getObject(params, function (err, data) {
+                    if (err) next(err);
+                    else {
+                        //const pathFile = path.join(__dirname, `./file/${fileName}`);
+                        memcached.set(fileName, data.Body.toString('base64'), 600, (err) => {
+                            if (err) {
+                                next(err);
+                            }
+                        });
+                        //let writeStr = fs.createWriteStream(pathFile);
+                        //writeStr.write(data.Body);
+                        if (!metadata) {
+                            metadata = {
+                                size: data.ContentLength,
+                                type: data.ContentType,
+                                encoding: data.ContentEncoding
+                            };
+                            client.hSet(fileName, 'type', data.ContentType);
+                            client.hSet(fileName, 'size', data.ContentLength);
+                            client.hSet(fileName, 'encoding', data.ContentEncoding);
                         }
-                    });
-                    //let writeStr = fs.createWriteStream(pathFile);
-                    //writeStr.write(data.Body);
-                    res.json({
-                        type: "no-cache",
-                        data: data.Body
-                    });
-                }
-            });
-        }
-    });
+                        res.json({
+                            type: "no-cache",
+                            data: data.Body,
+                            metadata: metadata
+                        });
+                    }
+                });
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get("/delete-file", (req, res, next) => {
@@ -273,18 +308,149 @@ app.get("/delete-file", (req, res, next) => {
         Bucket: process.env.BUCKET,
         Key: fileName
     };
-    s3.deleteObject(params, function (err, data) {
-        if (err) next(err);
-        else {
-            memcached.get(fileName, (err, data)=>{
-                if(err) return next(err);
-                memcached.del(fileName, (err)=>{
-                    if(err) return next(err);
+    s3.deleteObject(params, async function (err, data) {
+        try {
+            if (err) next(err);
+            else {
+                memcached.get(fileName, (err, data) => {
+                    if (err) return next(err);
+                    memcached.del(fileName, (err) => {
+                        if (err) return next(err);
+                    });
                 });
-            });
-            res.json("Success");
+                const metadata = await client.hGetAll(fileName);
+                if (metadata) {
+                    for (key in metadata) {
+                        client.hDel(fileName, key);
+                    }
+                }
+                res.json("Success");
+            }
+        } catch (error) {
+            next(error);
         }
     });
+});
+
+app.get("/get-listfile", (req, res, next) => {
+    try {
+        const s3 = new AWS.S3();
+        const params = {
+            Bucket: process.env.BUCKET,
+            MaxKeys: 20
+        };
+        s3.listObjects(params, (err, data) => {
+            if (err) return next(err);
+            const listKeys = data.Contents.map((file) => {
+                return file.Key;
+            });
+            res.json(listKeys);
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.post("/up-file-s3", uploadFile.any(), async (req, res, next) => {
+    try {
+        const s3 = new AWS.S3();
+        const resUrl = [];
+        let checkCache = false;
+        for (let i = 0; i < req.files.length; i++) {
+            console.log(req.files[i]);
+            if (req.files[i].size < 50971520) {
+                console.log("Up small size")
+                const params = { Bucket: process.env.BUCKET, Key: req.files[i].originalname, Body: req.files[i].buffer };
+                s3.upload(params, function (err, data) {
+                    console.log(data);
+                    if (err) return next(err);
+                    checkCache = true;
+                    resUrl.push(data.Location);
+                });
+            }
+            else {
+                console.log("Up big size");
+                const chunkSize = Math.pow(1024, 2) * 10;
+                const sizeFile = req.files[i].size;
+                const totalPart = Math.ceil(sizeFile / chunkSize);
+                const arrPart = Array.from(Array(totalPart).keys());
+                const uploadId = await createMultipart(s3, req.files[i].originalname);
+                console.log(uploadId)
+                const resParts = await Promise.allSettled(
+                    arrPart.map((part) => {
+                        uploadPart(s3, req.files[i].buffer.slice(part * chunkSize, (part + 1) * chunkSize), uploadId, part + 1, req.files[i].originalname);
+                    })
+                );
+                console.log(resParts);
+                const failParts = [];
+                const succeedParts = [];
+                for (let i = 0; i < resParts.length; i++) {
+                    if (resParts[i].status === "rejected") {
+                        failParts.push(i + 1);
+                    }
+                    else {
+                        const value = { ...resParts[i].value };
+                        succeedParts.push(value);
+                    }
+                }
+                if (failParts.length) {
+                    const retriedParts = await Promise.all(
+                        failParts.map((part) => {
+                            uploadPart(s3, req.files[i].buffer.slice((part - 1) * chunkSize, part * chunkSize), uploadId, part, req.files[i].originalname);
+                        })
+                    );
+                    succeedParts.push(...retriedParts);
+                }
+                const dataRes = await completeUpload(s3, uploadId, succeedParts.sort((a, b) => a.PartNumber - b.PartNumber), req.files[i].originalname);
+                if (!dataRes.Location) {
+                    await abortUpload(s3, uploadId, req.files[i].originalname);
+                }
+                else {
+                    checkCache = true;
+                    resUrl.push(dataRes.Location);
+                };
+            }
+
+            if (checkCache) {
+                memcached.set(req.files[i].originalname, req.files[i].buffer.toString('base64'), 600, (err) => {
+                    if (err) {
+                        next(err);
+                    }
+                });
+                client.hSet(req.files[i].originalname, 'type', req.files[i].mimetype);
+                client.hSet(req.files[i].originalname, 'size', req.files[i].size);
+                client.hSet(req.files[i].originalname, 'encoding', req.files[i].encoding);
+            }
+
+            if (i === req.files.length - 1) {
+                res.json(resUrl);
+            }
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.get("/get-total-size-bucket", (req, res, next) => {
+    const s3 = new AWS.S3();
+    let total = 0;
+    const params = {
+        Bucket: process.env.BUCKET,
+        MaxKeys: 20
+    };
+    s3.listObjects(params, async (err, data) => {
+        try {
+            if (err) return next(err);
+            for (let i = 0; i < data.Contents.length; i++) {
+                let size = await client.hGet(data.Contents[i].Key, 'size');
+                total = total + parseInt(size);
+            }
+            res.json(total);
+        } catch (error) {
+            next(error);
+        }
+    });
+
 });
 
 app.use((err, req, res) => {
---
+++ util/uploadMultipart.js
@@ -0,0 +1,72 @@
+const uploadPart = (s3, buffer, uploadId, partNumber, fileName) => {
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: fileName,
+        Body: buffer,
+        PartNumber: partNumber,
+        UploadId: uploadId
+    };
+    return new Promise((resolve, reject) => {
+        s3.uploadPart(params, (err, data) => {
+            console.log("Uploading...")
+            if (err) {
+                console.log(err)
+                reject(err);
+            }
+            else {
+                console.log("Success", data)
+                resolve({
+                    PartNumber: partNumber,
+                    ETag: data.ETag
+                });
+            }
+        });
+    });
+};
+
+const createMultipart = (s3, fileName) => {
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: fileName
+    };
+    return new Promise((resolve, reject) => {
+        s3.createMultipartUpload(params, (err, data) => {
+            if (err) reject(err);
+            else resolve(data.UploadId);
+        });
+    });
+};
+
+const abortUpload = (s3, uploadId, fileName) => {
+    const params = {
+        Bucket: process.env.BUCKET,
+        Key: fileName,
+        UploadId: uploadId
+    };
+    return new Promise((resolve, reject) => {
+        s3.abortMultipartUpload(params, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+const completeUpload = (s3, uploadId, parts, fileName) => {
+    const parmas = {
+        Bucket: process.env.BUCKET,
+        Key: fileName,
+        UploadId: uploadId,
+        MultipartUpload: {
+            Parts: parts
+        }
+    };
+    return new Promise((resolve, reject) => {
+        s3.completeMultipartUpload(parmas, (err, data) => {
+            console.log("Completed")
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+module.exports = { uploadPart, createMultipart, completeUpload, abortUpload };
